e7c1caef26a462143691e435861bfb0852544a78
root <root@localhost.localdomain>
2016-04-16 03:56:23 -0400

    buffer overflow example (partial)

---
+++ buffer_overflow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	int value = 5;
+	char buffer_one[8], buffer_two[8];
+	
+	strcopy(buffer_one, "one"); /* Put "one" into buffer_one. */
+	strcopy(buffer_two, "two"); /* Put "two" into buffer_two. */
+	
+	printf("[BEFORE] buffer_two is at %p and contains \'%s\'\n", buffer_two, buffer_two);
